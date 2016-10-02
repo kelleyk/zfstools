ca73d2171c33cb72c6943afbbc20018a1d1f22ec
Kevin Kelley <kelleyk@kelleyk.net>
2016-10-02 15:17:38 -0700

    Update Godeps.

--- Godeps/_workspace/src/github.com/kelleyk/gokk/regexp.go
+++ Godeps/_workspace/src/github.com/kelleyk/gokk/regexp.go
@@ -3,11 +3,13 @@ package gokk
 import "regexp"
 
 const (
+	// RFC3339Pattern is a regular expression pattern that matches partial strings in the format given by
+	// `time.RFC3339`, which is that specified in RFC 3339.  It contains no capturing groups.
 	RFC3339Pattern = `\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|(?:[+-]\d{2}:\d{2}))?`
 )
 
 var (
-	// dataset@zfs-auto-snap_label_ts
-	//   where ts format = e.g. `2006-01-02T15:04:05Z07:00`
+	// RFC3339Regexp is a regular expression that matches whole strings in the format given by `time.RFC3339`, which is
+	// that specified in RFC 3339.
 	RFC3339Regexp = regexp.MustCompile(`(?i)^(` + RFC3339Pattern + `)$`)
 )
